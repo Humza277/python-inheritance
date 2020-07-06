a4e870d6b814de7ad668a7523d1e00d44a95b956
hmalak <hmalak@spartaglobal.com>
2020-07-06 08:46:38 +0100

    comments

--- human.py
+++ human.py
@@ -1,10 +1,12 @@
-
+# parent class
 class Human:
+    # method initializes the variables
     def __init__(self, eye_color, skin_color, hair_color):
         self.eye_color = eye_color
         self.skin_color = skin_color
         self.hair_color = hair_color
 
+    # method prints out the variable
     def inputDetails(self):
         print("Your eye color is " + self.eye_color)
         print("Your skin color is " + self.skin_color)
--- person.py
+++ person.py
@@ -1,15 +1,16 @@
+# Imports the human file
 from human import *
 
 
-class Person(Human):
+class Person(Human):  # inherits characteristics from Human
     def __init__(self, eye_color, skin_color, hair_color, nose):
         super().__init__(eye_color, skin_color, hair_color)
-        self.nose = nose
+        self.nose = nose  # unique to person only
 
-    def printhuman(self):
+    def printhuman(self):  # method is unique to Person
         self.inputDetails()
         print("Your nose is " + self.nose)
 
 
-x = Person("Black", "Brown", "Blond", "crooked")
-print(x.printhuman())
+x = Person("Black", "Brown", "Blond", "crooked")  # inputs the variables for the characteristics
+print(x.printhuman())  # prints out the result
